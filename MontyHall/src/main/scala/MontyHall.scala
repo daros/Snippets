5889e772bf49271fc3d36540fdd69c3d44a47614
David Rosell <wmdaros@gmail.com>
2010-12-12 02:48:06 +0100

    Startegy added as traits

--- MontyHall/src/main/scala/MontyHall.scala
+++ MontyHall/src/main/scala/MontyHall.scala
@@ -10,14 +10,124 @@ import util.Random
  * Time: 03:38
  */
 
+object Hall extends Application {
+  val choices = List.range(1, 6)
+
+  println("Doors: "+choices)
+  // Changer
+  var wins = 0
+  for (i <- 1 to 10000) {
+    val hall = new Hall
+    val monty = new Monty(hall) with LeftDoor
+    val player = new Player with Changer
+
+    val playerChoice = player.choice(monty.choice(player.firstChoice))
+    if (playerChoice == hall.carDoor) wins += 1
+  }
+  println("changer wins: " + wins)
+
+  // Keeper
+  wins = 0
+  for (i <- 1 to 10000) {
+    val hall = new Hall
+    val monty = new Monty(hall) with LeftDoor
+    val player = new Player with Keeper
+
+    val playerChoice = player.choice(monty.choice(player.firstChoice))
+    if (playerChoice == hall.carDoor) wins += 1
+  }
+  println("keeper wins: " + wins)
+
+  // Fickle
+  wins = 0
+  for (i <- 1 to 10000) {
+    val hall = new Hall
+    val monty = new Monty(hall) with LeftDoor
+    val player = new Player with Fickle
+
+    val playerChoice = player.choice(monty.choice(player.firstChoice))
+    if (playerChoice == hall.carDoor) wins += 1
+  }
+  println("fickle wins: " + wins)
+
+  // Gambler
+  wins = 0
+  for (i <- 1 to 10000) {
+    val hall = new Hall
+    val monty = new Monty(hall) with LeftDoor
+    val player = new Player with Gambler
+
+    val playerChoice = player.choice(monty.choice(player.firstChoice))
+    if (playerChoice == hall.carDoor) wins += 1
+  }
+  println("gambler wins: " + wins)
+
+  // Gambler
+  wins = 0
+  for (i <- 1 to 10000) {
+    val hall = new Hall
+    val monty = new Monty(hall) with RandomDoor
+    val player = new Player with Gambler
+
+    val playerChoice = player.choice(monty.choice(player.firstChoice))
+    if (playerChoice == hall.carDoor) wins += 1
+  }
+  println("foolgambler wins: " + wins)
+}
+
 class Hall {
-  val carDoor = Random.nextInt(3) + 1
+  val carDoor = Random.shuffle(Hall.choices).head
 }
 
 class Player {
-  val firstChoise = Random.nextInt(3) + 1
+  val firstChoice = Random.shuffle(Hall.choices).head
 }
 
-class Monty(hall: Hall, player: Player) {
-  val open = 1
+class Monty(val hall: Hall) {
+}
+
+trait LeftDoor {
+  val hall: Hall
+  def choice(playerFirstChoice: Int) = {
+    Hall.choices.filter(_ != hall.carDoor).filter(_ != playerFirstChoice).head
+  }
+}
+
+trait RandomDoor {
+  val hall: Hall
+  def choice(playerFirstChoice: Int) = {
+    Random.shuffle(Hall.choices.filter(_ != hall.carDoor).filter(_ != playerFirstChoice)).head
+  }
+}
+
+trait Changer {
+  val firstChoice: Int
+
+  def choice(montyChoice: Int): Int = {
+    Hall.choices.filter(_ != montyChoice).filter(_ != firstChoice).head
+  }
+}
+
+trait Keeper {
+  val firstChoice: Int
+
+  def choice(montyChoice: Int): Int = firstChoice
+}
+
+trait Fickle {
+  val firstChoice: Int
+
+  def choice(montyChoice: Int): Int = {
+    Random.shuffle(Hall.choices.filter(_ != montyChoice)).head
+  }
+}
+
+trait Gambler {
+  val firstChoice: Int
+
+  def choice(montyChoice: Int): Int = {
+    if (Hall.choices.filter(_ != firstChoice).head == montyChoice) return firstChoice
+
+    Hall.choices.filter(_ != montyChoice).filter(_ != firstChoice).head
+  }
 }
--- MontyHall/src/test/scala/MontyHallSpec.scala
+++ MontyHall/src/test/scala/MontyHallSpec.scala
@@ -19,8 +19,8 @@ class MontyHallSpec extends Spec with ShouldMatchers {
         hall.carDoor should be(hall.carDoor)
       }
       it("should return the door in range") {
-        hall.carDoor > 0 should be(true)
-        hall.carDoor < 4 should be(true)
+        hall.carDoor >= Hall.choices.min should be(true)
+        hall.carDoor <= Hall.choices.max should be(true)
       }
     }
     describe("carDoor should be generated randomly") {
@@ -33,25 +33,30 @@ class MontyHallSpec extends Spec with ShouldMatchers {
         differentDoors should be(true)
       }
     }
+    describe("choices") {
+      it("should be at least List(1,2,3)") {
+        Hall.choices.size > 3  should be(true)
+      }
+    }
   }
 
   describe("Player") {
-    val player = new Player
     describe("first choise") {
+      val player = new Player
 
       it("should be remembered") {
-        player.firstChoise should be(player.firstChoise)
+        player.firstChoice should be(player.firstChoice)
       }
       it("should return the door in range") {
-        player.firstChoise > 0 should be(true)
-        player.firstChoise < 4 should be(true)
+        player.firstChoice >= Hall.choices.min should be(true)
+        player.firstChoice <= Hall.choices.max should be(true)
       }
 
       describe("should be random") {
         var differentDoors = false
         for (i <- 1 to 100) {
           val testPlayer = new Player
-          if (player.firstChoise != testPlayer.firstChoise) differentDoors = true
+          if (player.firstChoice != testPlayer.firstChoice) differentDoors = true
         }
         it("this should happend at least once probably") {
           differentDoors should be(true)
@@ -61,14 +66,79 @@ class MontyHallSpec extends Spec with ShouldMatchers {
   }
 
   describe("Monty") {
-    val hall = new Hall
-    val player = new Player
-    val monty = new Monty(hall, player)
-
     describe("opens door") {
-      it("other than carDoor and player firstChoise") {
-        monty.open should not be(hall.carDoor)
-        monty.open should not be(player.firstChoise)
+      val hall = new Hall
+      val player = new Player
+      val monty = new Monty(hall) with LeftDoor
+      val montyChoice = monty.choice(player.firstChoice)
+
+      it("other than carDoor") {
+        montyChoice should not be (hall.carDoor)
+      }
+      it("other than player firstChoice") {
+        montyChoice should not be (player.firstChoice)
+      }
+    }
+  }
+
+  describe("Player") {
+    describe("final choice") {
+      val player = new Player with Keeper
+      val hall = new Hall
+      val monty = new Monty(hall) with LeftDoor
+      val montyChoice = monty.choice(player.firstChoice);
+
+      it("should not be monty's choice") {
+        player.choice(montyChoice) should not be (montyChoice)
+      }
+    }
+
+    describe("final choice with changer strategy") {
+      val player = new Player with Changer
+      val hall = new Hall
+      val monty = new Monty(hall) with LeftDoor
+      val montyChoice = monty.choice(player.firstChoice);
+
+      it("should change") {
+        player.choice(montyChoice) should not be (player.firstChoice)
+      }
+    }
+
+    describe("final choice with keeper strategy") {
+      val player = new Player with Keeper
+      val hall = new Hall
+      val monty = new Monty(hall) with LeftDoor
+      val montyChoice = monty.choice(player.firstChoice);
+
+      it("should stay with first choice") {
+        player.choice(montyChoice) should be(player.firstChoice)
+      }
+    }
+
+    describe("final choice with fickle strategy") {
+      var player = new Player with Fickle
+      val hall = new Hall
+      val monty = new Monty(hall) with LeftDoor
+
+      var montyChoice = monty.choice(player.firstChoice);
+      var playerChoice = player.choice(montyChoice);
+
+      var differentChoices = false
+      val choiceOne = playerChoice == player.firstChoice
+      for (i <- 1 to 100) {
+        player = new Player with Fickle
+        montyChoice = monty.choice(player.firstChoice);
+        playerChoice = player.choice(montyChoice);
+
+        if (choiceOne) {
+          if (playerChoice != player.firstChoice) differentChoices = true
+        } else {
+          if (playerChoice == player.firstChoice) differentChoices = true
+        }
+      }
+
+      it("should changing") {
+        differentChoices should be(true)
       }
     }
   }
